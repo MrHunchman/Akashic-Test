85f41ab1fe214531e5e6e5e8720bf6e48fe5c807
OmbryalDev <naickerprajval47@gmail.com>
2026-06-22 12:06:19 +1200

    Add index.js to export multiple file builders

---
+++ js/exporters/index.js
@@ -0,0 +1,5 @@
+export { buildXML } from "./xml.js";
+export { buildCSV } from "./csv.js";
+export { buildJSON } from "./json.js";
+export { buildTXT } from "./txt.js";
+export { buildDOCX } from "./docx.js";
